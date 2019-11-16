fcc12a0b967a4924a20fa30e8b8bfed88aa2e458
Ilya Bogachev <bogachev_i_i@mail.ru>
2019-11-16 23:47:49 +0300

    add state to items

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({label, important=false}) => {
-    const style ={
-        color: important? 'steelblue': 'black',
-        fontWeight: important? 'bold': 'normal'
+export default class TodoListItem extends Component {
+    state={
+        done:false,
+        important:false
     }
-    return(<span className="todo-list-item">
+
+    onLabelClick = () =>
+        this.setState(({done})=>{
+            return{
+                done:!done
+            }
+        }
+    );
+
+    onThumbTack = () =>
+        this.setState(({important}) =>{
+            return{
+                important: !important
+            }
+        })
+
+    render(){
+       const {label} = this.props;
+       const {done, important} = this.state;
+       let classNames = 'todo-list-item';
+
+       if(done){
+           classNames+=' done';
+       }
+       if(important){
+           classNames+= ' important';
+       }
+        return(<span className={classNames}>
             <span className="todo-list-item-label"
-                 style={style}>
+                  onClick={this.onLabelClick}>
                 {label}
             </span>
-            <button type="button" className="btn btn-outline-success btn-sm">
+            <button type="button" className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onThumbTack}>
                 <i className="fa fa-thumb-tack" aria-hidden="true"/>
             </button>
-             <button type="button" className="btn btn-outline-danger btn-sm">
+             <button type="button" className="btn btn-outline-danger btn-sm float-right">
                 <i className="fa fa-recycle" aria-hidden="true"/>
             </button>
         </span>
-            )
-};
-
-export default TodoListItem;
+        )
+    }
+}
